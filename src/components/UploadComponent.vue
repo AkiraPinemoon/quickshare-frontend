d542c60d0a0d3f21cf93811400d21abaf8b4d13c
Akira Pinemoon <paulik080705@gmail.com>
2023-03-23 14:35:00 +0100

    added upload UI

--- src/components/UploadComponent.vue
+++ src/components/UploadComponent.vue
@@ -1,6 +1,21 @@
+
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const inputField = ref<HTMLInputElement>();
+
+const hasFile = ref(false);
+
+</script>
+
 <template>
-  <div class="w-80 h-40 rounded bg-gradient-to-br from-cyan-500 to-indigo-500 flex flex-col place-items-center justify-center gap-1">
+  <input @change="hasFile = true;" ref="inputField" type="file" class="hidden" />
+  <div v-if="!hasFile" @click="inputField?.click()" class="w-80 h-40 rounded bg-gradient-to-br from-cyan-500 to-indigo-500 flex flex-col place-items-center justify-center gap-1 hover:from-cyan-600 hover:to-indigo-600">
     <p class="text-3xl">Upload a File</p>
     <p class="text-sm">drop here or click to select</p>
+  </div>
+  <div v-else class="w-80 h-40 rounded bg-gradient-to-br from-cyan-500 to-indigo-500 flex flex-col place-items-center justify-center gap-1">
+    <p>Upload {{ inputField.files[0].name }}?</p>
   </div>
 </template>
