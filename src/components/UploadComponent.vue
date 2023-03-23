1de9e72e988fc84b3b4e9c2c4beb63e524c1d161
Akira Pinemoon <paulik080705@gmail.com>
2023-03-23 14:43:22 +0100

    bugfix

--- src/components/UploadComponent.vue
+++ src/components/UploadComponent.vue
@@ -7,15 +7,22 @@ const inputField = ref<HTMLInputElement>();
 
 const hasFile = ref(false);
 
+function updateFile() {
+  hasFile.value = true;
+  if( inputField.value?.files ) filename.value = inputField.value.files[0].name;
+}
+
+const filename = ref("");
+
 </script>
 
 <template>
-  <input @change="hasFile = true;" ref="inputField" type="file" class="hidden" />
+  <input @change="updateFile()" ref="inputField" type="file" class="hidden" />
   <div v-if="!hasFile" @click="inputField?.click()" class="w-80 h-40 rounded bg-gradient-to-br from-cyan-500 to-indigo-500 flex flex-col place-items-center justify-center gap-1 hover:from-cyan-600 hover:to-indigo-600">
     <p class="text-3xl">Upload a File</p>
     <p class="text-sm">drop here or click to select</p>
   </div>
   <div v-else class="w-80 h-40 rounded bg-gradient-to-br from-cyan-500 to-indigo-500 flex flex-col place-items-center justify-center gap-1">
-    <p>Upload {{ inputField.files[0].name }}?</p>
+    <p>Upload {{ filename }}?</p>
   </div>
 </template>
