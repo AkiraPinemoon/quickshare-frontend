0df5b0bd71b3371dad816ea9b4b51de9f04a5806
Paul Kunkel <paulik080705@gmail.com>
2023-05-14 20:53:48 +0200

    hotfix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 
 import axios from "axios";
 
-//const BASE_API_ROUTE = "/api/";
-const BASE_API_ROUTE = "http://192.168.178.165/api/";
+const BASE_API_ROUTE = "/api/";
+//const BASE_API_ROUTE = "http://192.168.178.165/api/";
 
 // checks if the id is an available file
 async function doesIdExist(id: string): Promise<boolean> {
